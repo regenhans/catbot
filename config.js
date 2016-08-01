35094596e7e200bffe896a61d32f6d408060889d
Hans <hans@hackergarage.mx>
2016-07-31 21:01:06 -0500

    your keys

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
+//use your own keys here (generate em on https://apps.twitter.com/)
 module.exports = {
-	consumer_key : 'qscJok41wCdmgNmhTssYLnGmn' ,
-	consumer_secret: 'gWWTNG4VhRUbkrrY7PxEcaa6egLYfLdHAFMkkBuF6fOBJN3SEy',
-	access_token: '236640161-hBulqqsORd23udNzEXPlwaDnVpexcfcEa1hGFvem',
-	access_token_secret: 'Xa3ooGfjhGvl6EGsW8QFV3imzhufwvC2X9SkCrIfxejui',
+	consumer_key : '---' ,
+	consumer_secret: '---',
+	access_token: '---',
+	access_token_secret: '---i',
 }
